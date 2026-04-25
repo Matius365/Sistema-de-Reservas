87d00d9eecbe7b1f8c8f63951f3eae61cd0abee2
Matius365 <macius365@gmail.com>
2026-04-25 13:54:01 +0200

    Conexion base de datos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.sql.Connection;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
+    //Comprobamos conexión a la base de datos
+//        try {
+//            Connection conn = ConexionDB.getConnection();
+//
+//            if (conn != null && !conn.isClosed()) {
+//                System.out.println("✅ Conexión exitosa a la base de datos");
+//            }
+//
+//            conn.close();
+//        } catch (Exception e) {
+//            System.out.println("❌ Error en la conexión");
+//            e.printStackTrace();
+//        }
     }
 }
