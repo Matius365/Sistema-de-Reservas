c4d93f7db1d76c6dc22746463eb61e74d17bfde0
Matius365 <macius365@gmail.com>
2026-04-27 13:41:47 +0200

    Creacion clases usuario y reserva

---
+++ src/Reserva.java
@@ -0,0 +1,136 @@
+import java.util.Date;
+
+public class Reserva {
+
+    private int idRecurso;
+
+    private int idReservaLocal;
+
+    private int idUsuario;
+
+    private Date fecha;
+
+    private String horaInicio;
+
+    private String horaFin;
+
+    private double coste;
+
+    private int numeroPlazas;
+
+    private String motivo;
+
+    private String observaciones;
+
+
+    //Constructor
+    public Reserva(int idRecurso, int idReservaLocal, int idUsuario, Date fecha,
+
+                   String horaInicio, String horaFin, double coste,
+
+                   int numeroPlazas, String motivo, String observaciones) {
+
+        this.idRecurso = idRecurso;
+
+        this.idReservaLocal = idReservaLocal;
+
+        this.idUsuario = idUsuario;
+
+        this.fecha = fecha;
+
+        this.horaInicio = horaInicio;
+
+        this.horaFin = horaFin;
+
+        this.coste = coste;
+
+        this.numeroPlazas = numeroPlazas;
+
+        this.motivo = motivo;
+
+        this.observaciones = observaciones;
+
+    }
+
+    // getters y setters
+    public int getIdRecurso() {
+        return idRecurso;
+    }
+
+    public void setIdRecurso(int idRecurso) {
+        this.idRecurso = idRecurso;
+    }
+
+    public int getIdReservaLocal() {
+        return idReservaLocal;
+    }
+
+    public void setIdReservaLocal(int idReservaLocal) {
+        this.idReservaLocal = idReservaLocal;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(String horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public double getCoste() {
+        return coste;
+    }
+
+    public void setCoste(double coste) {
+        this.coste = coste;
+    }
+
+    public int getNumeroPlazas() {
+        return numeroPlazas;
+    }
+
+    public void setNumeroPlazas(int numeroPlazas) {
+        this.numeroPlazas = numeroPlazas;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+}
---
+++ src/Usuario.java
@@ -0,0 +1,84 @@
+import java.util.Date;
+
+public class Usuario {
+
+        private int id;
+
+        private String correo;
+
+        private String contrasena;
+
+        private String nombre;
+
+        private Date fechaNacimiento;
+
+        private String tipo;
+
+        // Constructor
+        public Usuario(int id, String correo, String contrasena, String nombre, Date fechaNacimiento, String tipo) {
+
+            this.id = id;
+
+            this.correo = correo;
+
+            this.contrasena = contrasena;
+
+            this.nombre = nombre;
+
+            this.fechaNacimiento = fechaNacimiento;
+
+            this.tipo = tipo;
+
+        }
+
+        // Getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
